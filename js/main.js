db40fec2b1aa688b8474e4b17d50292ff514db16
aofur3957 <sunggin69@gmail.com>
2023-05-04 01:54:24 +0900

    review

--- js/main.js
+++ js/main.js
@@ -4,10 +4,14 @@ $(function () {
   const $btnClose = $('.btn-close');
   const $btnBrand = $('.btn-all');
   const $listBrand = $('.brand__list li');
+  const $btnPrevReview = $('.review-button-prev');
+  const $btnNextReview = $('.review-button-next');
+  const $reviewSlideItem = $('.review-slide .swiper-slide');
 
   function init(){
     watchEvent();
-    excuteSlide();
+    excuteMainSlide();
+    excuteReviewSlide()();
   }
 
   // 이벤트 감지
@@ -36,7 +40,7 @@ $(function () {
   }
 
   //슬라이드 
-  function excuteSlide(){
+  function excuteMainSlide(){
     $paging = ['ERS 출동 서비스', '4월에도 할인', '토스페이 캐시백', '친구 초대 이벤트', '브리지스톤 할인', '리뷰 이벤트', '앱 설치 혜택', '피렐리 무상교환', '삼성화재 이벤트', '자동세차 100원'];
 
     const swiperBullets = new Swiper('.visual-slide', {
@@ -72,5 +76,31 @@ $(function () {
     $btnBrand.toggleClass('active');
   }
 
+  // 리뷰
+  function excuteReviewSlide(){
+    const swiper = new Swiper('.review-slide', {
+      slidesPerView: 3,
+      spaceBetween: 24,
+      navigation: {
+        nextEl: '.review-button-next',
+        prevEl: '.review-button-prev',
+      },
+    }); 
+    return ()=>{
+      $btnNextReview.on('click', ()=>{
+        const length = $reviewSlideItem.length;
+        const currentIndex = swiper.activeIndex;
+        if(currentIndex === length-3) $btnNextReview.addClass('off');
+        if(currentIndex === 1) $btnPrevReview.removeClass('off');
+        
+      })
+      $btnPrevReview.on('click', ()=>{
+        const length = $reviewSlideItem.length;
+        const currentIndex = swiper.activeIndex;
+        if(currentIndex === 0) $btnPrevReview.addClass('off');
+        if(currentIndex === length-4) $btnNextReview.removeClass('off');
+      })
+    }
+  }
   init();
 });
